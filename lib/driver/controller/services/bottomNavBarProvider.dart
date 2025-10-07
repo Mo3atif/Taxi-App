bde89552e0451364f32c48136db7d67b6b159855
Mohamed Atif <hmode95271@gmail.com>
2025-10-07 12:41:07 -0700

    Delete lib directory

--- lib/driver/controller/services/bottomNavBarProvider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class BottomNavBarDriverProvider extends ChangeNotifier {
-  int currentTab = 0;
-  updateTab(int newTab) {
-    currentTab = newTab;
-    notifyListeners();
-  }
-}
